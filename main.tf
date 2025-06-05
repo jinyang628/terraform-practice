d6b65e2beb7834c765566b7ef50274a2ebae0811
jinyang628 <chenjinyang4192@gmail.com>
2025-06-05 21:08:58 +0800

    Delete outputs.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "github_repository" "terraform-example-repo-1" {
-  name               = "terraform-example-repo-1"
-  description        = "My first repository created with Terraform"
-  visibility         = "public"
-  auto_init          = true
-  license_template   = "mit"
+  name             = "terraform-example-repo-1"
+  description      = "My first repository created with Terraform"
+  visibility       = "public"
+  auto_init        = true
+  license_template = "mit"
 }
 
 resource "github_repository_ruleset" "example_ruleset_1" {
@@ -28,11 +28,11 @@ resource "github_repository_ruleset" "example_ruleset_1" {
 }
 
 resource "github_repository" "terraform-example-repo-2" {
-  name               = "terraform-example-repo-2"
-  description        = "My second repository created with Terraform"
-  visibility         = "public"
-  auto_init          = true
-  license_template   = "mit"
+  name             = "terraform-example-repo-2"
+  description      = "My second repository created with Terraform"
+  visibility       = "public"
+  auto_init        = true
+  license_template = "mit"
 }
 
 resource "github_repository_ruleset" "example_ruleset_2" {
