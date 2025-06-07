48b1a80f01ff1dc5cbe02709d04625e41ea8f0d8
jinyang628 <chenjinyang4192@gmail.com>
2025-06-07 14:39:24 +0800

    Add comments to gh actions

--- scripts/import.sh
+++ scripts/import.sh
@@ -6,5 +6,5 @@ terraform state list | grep "github_repository" | while read -r tf_resource; do
   repo_name=$(terraform state show "$tf_resource" | grep -oP 'name\s+=\s+"\K[^"]+')
   
   echo "Importing $repo_name as $tf_resource..."
-  terraform import "$tf_resource" "$repo_name" || echo "Skipping $repo_name (may already exist)"
+  terraform import "$tf_resource" "$repo_name" || echo "$repo_name doesn't exist yet - will be created during apply"
 done
