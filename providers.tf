1bda04424ae4bf7f46212ee81b13ef231cc8168b
jinyang628 <chenjinyang4192@gmail.com>
2025-06-07 16:07:45 +0800

    Trigger terraform cloud to update remote state

--- providers.tf
+++ providers.tf
@@ -5,9 +5,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "remote" {
+    organization = "jy-terraform" # Configured within terraform cloud (https://app.terraform.io)
+    workspaces {
+      name = "terraform-practice"
+    }
+  }
 }
 
 provider "github" {
-  owner = "jy-terraform"
+  owner = "jy-terraform" # Name of the GitHub organization
   token = var.github_token
 }
