be828268b6a7639be3273111d284acb48b9569cb
jinyang628 <chenjinyang4192@gmail.com>
2025-06-07 15:25:11 +0800

    Set stricter merge requirements

--- repositories.tf
+++ repositories.tf
@@ -6,18 +6,24 @@ resource "github_repository" "terraform-example-repo-1" {
   name                        = "terraform-example-repo-1"
   description                 = "My first repository created with Terraform"
   visibility                  = "public"
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
   allow_squash_merge          = true
   squash_merge_commit_title   = "PR_TITLE"
   squash_merge_commit_message = "BLANK"
   delete_branch_on_merge      = true
+  auto_init                   = true
 }
 
 resource "github_repository" "terraform-example-repo-2" {
   name                        = "terraform-example-repo-2"
   description                 = "My second repository created with Terraform"
   visibility                  = "public"
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
   allow_squash_merge          = true
   squash_merge_commit_title   = "PR_TITLE"
   squash_merge_commit_message = "BLANK"
   delete_branch_on_merge      = true
+  auto_init                   = true
 }
